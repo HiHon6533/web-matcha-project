d31cb8c957b4877c659d251367f4e7ff64651835
Nguyễn Phúc Huy Hoàng <23110100@student.hcmute.edu.vn>
2025-12-27 15:07:27 +0700

    CAP NHAT DAO

--- src/main/java/dao/IngredientDAO.java
+++ src/main/java/dao/IngredientDAO.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author ADMIN
- */
-public class IngredientDAO {
-    
+import model.Ingredient;
+
+public class IngredientDAO extends GenericDAO<Ingredient, Long> {
+
+    public IngredientDAO() {
+        super(Ingredient.class);
+    }
 }
