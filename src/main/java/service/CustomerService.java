b52d13da0199fa3608d5b07a0fbedc8b5999ca46
NHP39 <23110139@student.hcmute.edu.vn>
2025-12-27 20:57:15 +0700

    Service-base

---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,26 @@
+package service;
+
+
+public class CustomerService {
+    
+    //UPDATE PROFILE
+    public void updateProfile(Long userID, UpdateProfileDTO dto){
+    
+    }
+    //ADD ADDRESS
+    public void updateProfile(Long userID, UpdateProfileDTO dto){
+    
+    }
+    //DELETE ADDRESS
+    public void addAddress(Long userID, AddressDTO dto){
+    
+    }
+    //SET DAFAULT ADDRESS
+    public void setDefaultAddress(Long userID, Long addressID){
+        
+    }
+    //GET CUSTOMER BY USERID
+    public Customer getCustomerByUserID(Long userID){
+        
+    }
+}
